3cfc9f009c6e61f62224937309faaacba9c94eec
miruat <36480281+miruat@users.noreply.github.com>
2019-06-03 20:13:43 +0200

    setup

---
+++ desktop/src/com/shardbytes/dungeon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.shardbytes.dungeon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.shardbytes.dungeon.DungeonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DungeonGame(), config);
+	}
+}
